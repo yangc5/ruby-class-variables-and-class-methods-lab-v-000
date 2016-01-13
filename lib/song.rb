f463cec0916dbc17bd2c69a2d8979c87cfaa88d9
Chen Yang <yangc5@mail.uc.edu>
2016-01-13 02:42:06 +0000

    This code doesn't work well with the test however no error when running the code standalone

--- lib/song.rb
+++ lib/song.rb
@@ -1,17 +1,34 @@
+require 'pry'
+
 class Song
-  attr_accessor :name, :artist, :genre
+  attr_reader :name, :artist, :genre
 
   @@count = 0
   @@artists = []
   @@genres = []
+  @@artist_count = {}
+  @@genre_count = {}
 
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
-    @@count +=1
-    @@genres << genre
-    @@artists << artist
+
+    @@count += 1
+    @@genres << @genre
+    if @@genre_count.keys.include?(@genre)
+      @@genre_count[@genre] +=1
+    else
+      @@genre_count[@genre] = 1
+    end
+
+    @@artists << @artist
+    if @@artist_count.keys.include?(@artist)
+      @@artist_count[@artist] +=1
+    else
+      @@artist_count[@artist] = 1
+    end
+
   end
 
   def self.count
@@ -22,31 +39,23 @@ class Song
     @@artists.uniq
   end
 
-  def self.artist_count
-    artist_count = {}
-    @@artists.each do |artist|
-      if artist_count[artist]
-        artist_count[artist] += 1 
-      else
-        artist_count[artist] = 1
-      end
-    end
-    artist_count
-  end
-
   def self.genres
     @@genres.uniq
   end
 
   def self.genre_count
-    genre_count = {}
-    @@genres.each do |genre|
-      if genre_count[genre]
-        genre_count[genre] += 1 
-      else
-        genre_count[genre] = 1
-      end
-    end
-    genre_count
+    @@genre_count
+  end
+
+  def self.artist_count
+    @@artist_count
   end
 end
+
+a = Song.new('a', 'b', 'c')
+b = Song.new('d', 'e', 'f')
+c = Song.new('a', 'd', 'f')
+puts Song.artist_count
+puts Song.genre_count
+
+# binding.pry
